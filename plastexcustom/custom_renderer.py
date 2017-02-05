360009fdf0af987a7bfcae2c12bbdc28a76bae2c
gk <gabriel.kabbe@mail.de>
2017-02-05 17:12:49 +0100

    Ignore Latex macros, remove non alphanumeric character from XML tags

--- plastexcustom/custom_renderer.py
+++ plastexcustom/custom_renderer.py
@@ -12,11 +12,18 @@ class Renderer(Renderer):
         if len(node.nodeName) == 1 and node.nodeName not in string.letters:
             return self.textDefault(node.nodeName)
 
+        # Handle Latex macros
+        if node.macroName:
+            return self.textDefault(node.nodeName)
+
         # Start tag
         s.append('<%s>' % node.nodeName)
         # See if we have any attributes to render
         if node.hasAttributes():
             for key, value in node.attributes.items():
+                # Remove non alphanumeric characters
+                if not key.isalnum():
+                    key = "".join(char for char in key if char.isalnum())
                 # If the key is 'self', don't render it
                 # these nodes are the same as the child nodes
                 if key == 'self':
