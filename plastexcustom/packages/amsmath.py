fc3cd66e5ca1540aa4aabf8e4cb4ef9a58326430
gk <gabriel.kabbe@mail.de>
2017-02-22 09:51:11 +0100

    Change amsmath

--- plastexcustom/packages/amsmath.py
+++ plastexcustom/packages/amsmath.py
@@ -2,28 +2,28 @@ from plasTeX import Base
 
 
 class math(Base.Environment):
-    args = "formula:str"
+    args = "formula"
 
     def invoke(self, tex):
         Base.Command.invoke(self, tex)
 
 
 class displaymath(Base.Environment):
-    args = "formula:str"
+    args = "formula"
 
     def invoke(self, tex):
         Base.Command.invoke(self, tex)
 
 
 class eqnarray(Base.Environment):
-    args = "formula:str"
+    args = "formula"
 
     def invoke(self, tex):
         Base.Command.invoke(self, tex)
 
 
 class equation(Base.Environment):
-    args = "formula:str"
+    args = "formula"
 
     def invoke(self, tex):
         Base.Command.invoke(self, tex)
