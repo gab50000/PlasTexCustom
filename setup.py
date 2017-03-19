39d4acb38808d92d362472ca81df52aea6828876
gk <gabriel.kabbe@mail.de>
2017-03-19 18:30:51 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='custex',
       version='0.1',
-      description='Custom PlasTeX',
+      description='Customized PlasTeX',
       author='Gabriel Kabbe',
       author_email='gabriel.kabbe@mail.de',
       license='GPLv3',
@@ -12,7 +12,8 @@ setup(name='custex',
                         "nose"],
       entry_points={
                     'console_scripts': ["tex2xml=plastexcustom.main:main",
-                                        "prettify=plastexcustom.make_pretty:main"
+                                        "prettify=plastexcustom.make_pretty:main",
+                                        "treeview=plastexcustom.TexTree:main"
                                         ],
       },
       zip_safe=False)
