7b1ca9390239396c7de80e8dca288dbdaf67c055
gk <gabriel.kabbe@mail.de>
2017-03-21 10:28:14 +0100

    Update scrbook

--- plastexcustom/packages/scrbook.py
+++ plastexcustom/packages/scrbook.py
@@ -5,18 +5,3 @@ class pleibvdash(Base.Command):
     args = "self"
     def digest(self, tokens):
         self.image_path = u"images/abc"
-
-
-class footnotesize(Base.Command):
-    args = ""
-
-    def digest(self, tokens):
-        Base.Command.digest(self, tokens)
-        self.attributes["formatting"] = {"font": "footnotesize"}
-
-
-class tiny(Base.Command):
-    args = ""
-
-    def digest(self, tokens):
-        self.attributes["formatting"] = {"font": "tiny"}
