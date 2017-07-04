1872a98c6ab140f87027ded659a41fef34ab55f6
gk <gabriel.kabbe@mail.de>
2017-07-04 23:31:04 +0200

    Update main

--- plastexcustom/main.py
+++ plastexcustom/main.py
@@ -2,6 +2,8 @@
 """Hauptskript, das zuerst den Parser aufruft, und anschließend den Dokumentenbaum mithilfe des
 custom_renderers sowie der hier definierten Funktionen rendert."""
 
+import argparse
+import logging
 import sys
 import os
 import codecs
@@ -24,6 +26,8 @@ c.add_section("debugging")
 c["debugging"]["verbose"] = "True"
 
 
+logger = logging.getLogger(__name__)
+
 #---------------------------------------------------------------------------------------------------
 # Hier werden die Funktionen definiert, die das XML Ausgabeformat für jeden Latex-Befehl bestimmen.
 
@@ -39,6 +43,7 @@ def convert_edtext(node):
 def do_nothing(node):
     """Diese Funktion gibt einen leeren (Unicode-) String zurück.
     Kann benutzt werden, um zu verhindern, dass unnötige Elemente in der XML-Datei auftauchen."""
+    logger.debug(u"Return empty string for node {}".format(node.nodeName))
     return u''
 
 
@@ -49,11 +54,14 @@ def debug(node):
 def do_not_write_tags(node):
     """Schreibt den Inhalt des aktuellen Environments/Befehls nach XML, aber erzeugt keine Tags
     dafür"""
+    logger.debug(u"Do not write tags for node {}".format(node.nodeName))
     return unicode(node)
 
 
 def convert_graphics(node):
-    return u"<graphics>\n<file>\n{}\n</file>\n</graphics>".format(unicode(node.attributes["file"]))
+    filename = unicode(node.attributes["file"])
+    logger.debug(u"Convert {} to Image {}".format(node.nodeName, filename))
+    return u"<graphics>\n<file>\n{}\n</file>\n</graphics>".format(filename)
 
 
 def equation_as_latexsrc_with_image(node):
@@ -74,16 +82,12 @@ def equation_as_latexsrc(node):
 
 def open_paragraph(node):
     """Öffnet einen Paragraphen mittels <p>"""
-    parents = get_parents(node)
-    style = find_formatter_class(parents)
-    if style:
-        return u'<p size="{}">'.format(style)
-    return u"<p>"
+    return u"\n<p>\n"
 
 
-def end_paragraph(node):
+def close_paragraph(node):
     """Schließt einen Paragraphen mittels </p>"""
-    return u"</p>"
+    return u"\n</p>\n"
 
 
 def textsize(node):
@@ -94,6 +98,15 @@ def textsize(node):
 
 
 def main(*args):
+    parser = argparse.ArgumentParser("Convert tex to XML")
+    parser.add_argument("filename", help="Latex-Filename")
+    parser.add_argument("--pretty", action="store_true", help="Prettify output")
+    parser.add_argument("--loglevel", "-l", default="INFO",
+                        choices=["info", "INFO", "debug", "DEBUG", "warn", "WARN"],
+                        help="Set log level")
+    args = parser.parse_args()
+
+    logger.setLevel(getattr(logging, args.loglevel.upper()))
     # Determine name of XML output
     filename_root, ext = os.path.splitext(sys.argv[1])
     xml_filename = filename_root + ".xml"
@@ -104,6 +117,7 @@ def main(*args):
     tex.ownerDocument.config['files']['filename'] = xml_filename
     tex.ownerDocument.config['images']['imager'] = 'gspdfpng'
     tex.ownerDocument.config['images']['filenames'] = 'xmlimages/img-$num(4)'
+    tex.ownerDocument.config['images']['save-file'] = "on"
 
     with codecs.open(sys.argv[1], "r", encoding="utf-8") as f:
         file_content = f.read()
@@ -118,14 +132,21 @@ def main(*args):
     textsizes = ["tiny", "scriptsize", "footnotesize", "small", "normalsize", "large", "Large",
                  "LARGE", "huge", "Huge"]
 
+    other = ["count", "setcounter"]
+
     for ts in textsizes:
         renderer[ts] = do_not_write_tags
 
+    for ot in other:
+        renderer[ot] = do_not_write_tags
+
     # Alle Latex-Befehle, die in der folgenden Liste stehen, werden nicht ins XML-Dokument
     # geschrieben
+    # Achtung: gilt auch für alle enthaltenen Kindknoten
     # Bei Bedarf ergänzen!
 
-    to_be_ignored = ["vspace", "renewcommand", "pstart", "pend"]
+    to_be_ignored = ["vspace", "renewcommand"]
+
     for tbi in to_be_ignored:
         renderer[tbi] = do_nothing
 
@@ -137,12 +158,16 @@ def main(*args):
 
     renderer["edtext"] = convert_edtext
     renderer["includegraphics"] = convert_graphics
+    renderer["pstart"] = open_paragraph
+    renderer["pend"] = close_paragraph
 
     # Render the document
     renderer.render(document)
 
-    # Make XML pretty
-    parser = etree.XMLParser(remove_blank_text=False)
-    tree = etree.parse(xml_filename, parser)
-    tree.write(xml_filename, pretty_print=True)
+    if args.pretty:
+        logger.info("Make XML document pretty")
+        # Make XML pretty
+        parser = etree.XMLParser(remove_blank_text=False)
+        tree = etree.parse(xml_filename, parser)
+        tree.write(xml_filename, pretty_print=True)
 
